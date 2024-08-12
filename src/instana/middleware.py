38f06d6fc8c80ef1a8dc6972f16b8b9cfb8a027e
Paulo Vital <paulo.vital@ibm.com>
2024-08-12 16:55:46 +0200

    refactor: ASGI instrumentation
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/middleware.py
+++ src/instana/middleware.py
@@ -2,5 +2,5 @@
 # (c) Copyright Instana Inc. 2017
 
 
-from .instrumentation.wsgi import InstanaWSGIMiddleware
-from .instrumentation.asgi import InstanaASGIMiddleware
+# from instana.instrumentation.wsgi import InstanaWSGIMiddleware  # noqa: F401
+from instana.instrumentation.asgi import InstanaASGIMiddleware  # noqa: F401
